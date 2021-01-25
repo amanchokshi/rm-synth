df7f74be6ce0e904f9b58c80d9413fc511033b0f
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-01-25 20:12:15 +0800

    Added bash variables to easily reuse script

--- wsclean/sbatch_uv2ms.sh
+++ wsclean/sbatch_uv2ms.sh
@@ -10,6 +10,14 @@
 #SBATCH --output=/astro/mwaeor/MWA/data/1061316296/2021-01-20_2000/uv2ms-%A.out
 #SBATCH --error=/astro/mwaeor/MWA/data/1061316296/2021-01-20_2000/uv2ms-%A.err
 
+
+obsid=1061316296
+timestamp=2021-01-20_2000
+data_dir=/astro/mwaeor/MWA/data
+out_dir="$data_dir"/"$obsid"/"$timestamp"/ms
+prefix=uvdump_
+
+
 module use /pawsey/mwa/software/python3/modulefiles
 module load casa/5.6.1-8
 module load cotter/v4.3
@@ -17,14 +25,15 @@ module load intel-mkl/19.0.5
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pawsey/intel/19.0.5/mkl/lib/intel64/
 
-cd /astro/mwaeor/MWA/data/1061316296/2021-01-20_2000/
 
-time casa --nologger -c /astro/mwaeor/achokshi/rm-synth/wsclean/uv2ms.py uvdump_ /astro/mwaeor/MWA/data/1061316296/2021-01-20_2000/1061316296_metafits_ppds.fits
+cd "$data_dir"/"$obsid"/"$timestamp"
+
+time casa --nologger -c /astro/mwaeor/achokshi/rm-synth/wsclean/uv2ms.py $prefix "$data_dir"/"$obsid"/"$timestamp"/"$obsid"_metafits_ppds.fits
 
 
 time for band in '01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' '20' '21' '22' '23' '24'
 do
-  fixmwams uvdump_${band}.ms /astro/mwaeor/MWA/data/1061316296/2021-01-20_2000/1061316296_metafits_ppds.fits
+  fixmwams "$prefix"${band}.ms "$data_dir"/"$obsid"/"$timestamp"/"$obsid"_metafits_ppds.fits
 done
 
-mkdir ms && mv *.ms ms
+mkdir -p $out_dir && mv "$data_dir"/"$obsid"/"$timestamp"/*.ms $out_dir
