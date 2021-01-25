0c8f3c810ebbbf04b679510ae9636f6cc1dd8517
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-01-25 20:40:18 +0800

    Coarseband imaging working

--- wsclean/sbatch_wsclean.sh
+++ wsclean/sbatch_wsclean.sh
@@ -14,14 +14,23 @@ module load wsclean
 
 cd /astro/mwaeor/MWA/data/1061316296/2021-01-20_2000
 
-mkdir -p images_i
+mkdir -p images_ii
 
-time wsclean -name ./images_i/uvdump -size 1024 1024 -niter 10000 \
-  -auto-threshold 0.5 -auto-mask 3 \
+# time wsclean -name ./images_i/uvdump -size 1024 1024 -niter 10000 \
+  # -auto-threshold 0.5 -auto-mask 3 \
+  # -pol I -multiscale -weight briggs 0 -scale 0.033 -j 20 -mgain 0.85 \
+  # -no-update-model-required \
+  # -abs-mem 100 \
+  # -grid-with-beam -use-idg -idg-mode cpu -pb-undersampling 4 \
+  # -mwa-path /astro/mwaeor/achokshi/software/local_python/mwa_pb/data \
+  # /astro/mwaeor/MWA/data/1061316296/2021-01-20_2000/ms/uvdump_*.ms
+  # #-channels-out 4 -join-channels \
+
+time wsclean -name ./images_ii/uvdump -size 1024 1024 -niter 10000 \
+  -auto-threshold 0.5 -auto-mask 3 -channels-out 24 \
   -pol I -multiscale -weight briggs 0 -scale 0.033 -j 20 -mgain 0.85 \
   -no-update-model-required \
   -abs-mem 100 \
   -grid-with-beam -use-idg -idg-mode cpu -pb-undersampling 4 \
   -mwa-path /astro/mwaeor/achokshi/software/local_python/mwa_pb/data \
   /astro/mwaeor/MWA/data/1061316296/2021-01-20_2000/ms/uvdump_*.ms
-  #-channels-out 4 -join-channels \
