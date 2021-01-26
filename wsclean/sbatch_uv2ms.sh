ea538d23cd4865de955479f3725a6034b96a5acc
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-01-26 18:12:24 +0800

    Moved uv2ms.py to python-scripts dir

--- wsclean/sbatch_uv2ms.sh
+++ wsclean/sbatch_uv2ms.sh
@@ -28,7 +28,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pawsey/intel/19.0.5/mkl/lib/intel64/
 
 cd "$data_dir"/"$obsid"/"$timestamp"
 
-time casa --nologger -c /astro/mwaeor/achokshi/rm-synth/wsclean/uv2ms.py $prefix "$data_dir"/"$obsid"/"$timestamp"/"$obsid"_metafits_ppds.fits
+time casa --nologger -c /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/uv2ms.py $prefix "$data_dir"/"$obsid"/"$timestamp"/"$obsid"_metafits_ppds.fits
 
 
 time for band in '01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' '20' '21' '22' '23' '24'
