0a065bfb009d5cf13e5ed890b8df940d2a96c490
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-01-27 19:58:49 +0800

    Create cubes from cleaned images

--- wsclean/sbatch_fits2cube.sh
+++ wsclean/sbatch_fits2cube.sh
@@ -14,8 +14,9 @@
 obsid=1061316296
 timestamp=2021-01-27_1200
 data_dir=/astro/mwaeor/MWA/data
-fits_dir="$data_dir"/"$obsid"/"$timestamp"/images
+fits_dir="$data_dir"/"$obsid"/"$timestamp"/images_clean
 prefix=uvdump
+suffix=image
 chans=768
 dim=1024
 
@@ -23,9 +24,8 @@ module load python
 module load numpy
 module load astropy
 
-# Use local python modules
-export PYTHONPATH=$PYTHONPATH:/astro/mwaeor/achokshi/software/local_python
+time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
+    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=Q --chans=$chans --dim=$dim --out_dir=$fits_dir
 
-time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py --fits_dir=$fits_dir --prefix=$prefix --pol=Q --chans=$chans --dim=$dim --out_dir=$fits_dir
-
-time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py --fits_dir=$fits_dir --prefix=$prefix --pol=U --chans=$chans --dim=$dim --out_dir=$fits_dir
+time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
+    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=U --chans=$chans --dim=$dim --out_dir=$fits_dir
