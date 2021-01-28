93635b480122f44fc0d1157c3cf5317d9f3a62d0
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-01-28 18:05:27 +0800

    Save cubes to rm_cubes dir

--- wsclean/sbatch_fits2cube.sh
+++ wsclean/sbatch_fits2cube.sh
@@ -7,25 +7,29 @@
 #SBATCH --mem=10gb
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --output=/astro/mwaeor/MWA/data/1061316296/2021-01-27_1200/fits2cube-%A.out
-#SBATCH --error=/astro/mwaeor/MWA/data/1061316296/2021-01-27_1200/fits2cube-%A.err
+#SBATCH --output=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/fits2cube-%A.out
+#SBATCH --error=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/fits2cube-%A.err
 
 
 obsid=1061316296
-timestamp=2021-01-27_1200
+timestamp=2021-01-28_2000
 data_dir=/astro/mwaeor/MWA/data
-fits_dir="$data_dir"/"$obsid"/"$timestamp"/images_clean
+fits_dir="$data_dir"/"$obsid"/"$timestamp"/ws_rm
 prefix=uvdump
-suffix=dirty
+suffix=image
 chans=768
-dim=1024
+dim=2048
 
 module load python
 module load numpy
 module load astropy
 
-time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
-    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=Q --chans=$chans --dim=$dim --out_dir=$fits_dir
+mkdir -p "$data_dir"/"$obsid"/"$timestamp"/rm_cubes
 
 time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
-    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=U --chans=$chans --dim=$dim --out_dir=$fits_dir
+    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=Q --chans=$chans --dim=$dim --out_dir="$data_dir"/"$obsid"/"$timestamp"/rm_cubes
+
+
+time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
+    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=U --chans=$chans --dim=$dim --out_dir="$data_dir"/"$obsid"/"$timestamp"/rm_cubes
+
