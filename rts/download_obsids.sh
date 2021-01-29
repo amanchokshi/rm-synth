5f2ffe02d5774a15e58b0089fb0334c93cbffb12
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-01-30 03:04:57 +1100

    Read json input file

--- rts/download_obsids.sh
+++ rts/download_obsids.sh
@@ -14,48 +14,53 @@
 module use /pawsey/mwa/software/python3/modulefiles
 module load giant-squid
 
+module use /astro/mwaeor/achokshi/software/modulefiles
+module load jq
+
 sub_dir=gpubox
-mwa_dir=/astro/mwaeor/MWA/data/
+mwa_dir=/astro/mwaeor/MWA/data
 obs_file_name=/astro/mwaeor/achokshi/rm-synth/rts/obsids.txt
 
 #Read the obs file and put into an array, skipping blank lines if they exist
-i=0
-while read line
-do
-   if [ ! -z "$line" ]; then
-      obs_id_array[$i]=$line
-      i=$((i + 1))
-   fi
-done < "$obs_file_name"
+# i=0
+# while read line
+# do
+   # if [ ! -z "$line" ]; then
+      # obs_id_array[$i]=$line
+      # i=$((i + 1))
+   # fi
+# done < "$obs_file_name"
+
+obs_id_array=$(jq '.obsids | .[]' rts_in.json)
 
 for obs_id in "${obs_id_array[@]}"
 do
-   giant-squid submit-vis $obs_id
+   giant-squid submit-vis "$obs_id"
 done
 
 
 for obs_id in "${obs_id_array[@]}"
 do
-   mkdir -p ${mwa_dir}${obs_id}/${sub_dir}
-   cd ${mwa_dir}${obs_id}/${sub_dir}
+   mkdir -p "${mwa_dir}"/"${obs_id}"/"${sub_dir}"
+   cd "${mwa_dir}"/"${obs_id}"/"${sub_dir}"
 
-   num_files=$(ls ${mwa_dir}${obs_id}/${sub_dir}/${obs_id}* | wc -l)
+   num_files=$(ls "${mwa_dir}"/"${obs_id}"/"${sub_dir}"/"${obs_id}"* | wc -l)
 
    if [[ $num_files -gt 47 ]]; then
-       echo $obs_id was already downloaded
+       echo "$obs_id" was already downloaded
    else
        download_flag=0
        while [ $download_flag -eq 0 ]
        do
-           message=$(giant-squid download ${obs_id})
+           message=$(giant-squid download "${obs_id}")
 
            if [[ $message == *"not ready"* ]]; then
                sleep 60
            else
-               unzip ${mwa_dir}${obs_id}/${sub_dir}/${obs_id}_flags.zip
-               rm ${mwa_dir}${obs_id}/${sub_dir}/${obs_id}_flags.zip
+               unzip "${mwa_dir}"/"${obs_id}"/"${sub_dir}"/"${obs_id}"_flags.zip
+               rm "${mwa_dir}"/"${obs_id}"/"${sub_dir}"/"${obs_id}"_flags.zip
                download_flag=1
-               echo Successfully downloaded ${obs_id}
+               echo Successfully downloaded "${obs_id}"
            fi
        done
    fi
