44a82b61bc09d1a935cbbfc6f058f63969330182
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-02-01 18:47:11 +0800

    Work with new data tree

--- wsclean/sbatch_fits2cube.sh
+++ wsclean/sbatch_fits2cube.sh
@@ -1,4 +1,5 @@
 #!/bin/bash --login
+
 #SBATCH --nodes=1
 #SBATCH --partition=workq
 #SBATCH --time=00:30:00
@@ -7,29 +8,29 @@
 #SBATCH --mem=30gb
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --output=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/fits2cube-%A.out
-#SBATCH --error=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/fits2cube-%A.err
+#SBATCH --output=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/fits2cube-%A.out
+#SBATCH --error=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/fits2cube-%A.err
 
 
 obsid=1061316296
-timestamp=2021-01-28_2000
-data_dir=/astro/mwaeor/MWA/data
-fits_dir="$data_dir"/"$obsid"/"$timestamp"/ws_rm
+tag=POGSII-EG-024
+data_dir=/astro/mwaeor/achokshi/rm-synth/data
+fits_dir="$data_dir"/"$obsid"/"$tag"/wsclean_rm
 prefix=uvdump
-suffix=dirty
+suffix=image
 chans=768
-dim=2048
+dim=256
+
 
 module load python
 module load numpy
 module load astropy
 
-mkdir -p "$data_dir"/"$obsid"/"$timestamp"/rm_cubes
+mkdir -p "$data_dir"/"$obsid"/"$tag"/rm_cubes
 
 time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
-    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=Q --chans=$chans --dim=$dim --out_dir="$data_dir"/"$obsid"/"$timestamp"/rm_cubes
+    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=Q --chans=$chans --dim=$dim --out_dir="$data_dir"/"$obsid"/"$tag"/rm_cubes
 
 
 time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
-    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=U --chans=$chans --dim=$dim --out_dir="$data_dir"/"$obsid"/"$timestamp"/rm_cubes
-
+    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=U --chans=$chans --dim=$dim --out_dir="$data_dir"/"$obsid"/"$tag"/rm_cubes
