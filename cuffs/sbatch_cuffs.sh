4167f07797638ab5b6ad6db7729f81958e162381
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-02-01 19:10:17 +0800

    Works with new data tree

--- cuffs/sbatch_cuffs.sh
+++ cuffs/sbatch_cuffs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -l
+
 #SBATCH --job-name="cuFFS"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 #SBATCH --clusters=garrawarla
 #SBATCH --partition=gpuq
 #SBATCH --account=mwaeor
 #SBATCH --gres=gpu:1
-#SBATCH --output=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/cuffs-%A.out
-#SBATCH --error=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/cuffs-%A.err
+#SBATCH --output=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/cuffs-%A.out
+#SBATCH --error=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/cuffs-%A.err
 
 module use /pawsey/mwa/software/python3/modulefiles
 module load RTS/sla_to_pal
