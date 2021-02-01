69a9eaa8b74bcdbc6a5fe45d07c597e7115b6a53
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-02-01 18:38:45 +0800

    wsclean trial for POGSII-EG-024

--- wsclean/sbatch_wsclean_im.sh
+++ wsclean/sbatch_wsclean_im.sh
@@ -1,32 +1,32 @@
 #!/bin/bash --login
 #SBATCH --nodes=1
 #SBATCH --partition=workq
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH --account=mwaeor
 #SBATCH --nodes=1
 #SBATCH --mem=100gb
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --output=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/wsclean-%A.out
-#SBATCH --error=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/wsclean-%A.err
+#SBATCH --output=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/wsclean-im-%A.out
+#SBATCH --error=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/wsclean-im-%A.err
 
 module load wsclean
 
 obsid=1061316296
-timestamp=2021-01-28_2000
-data_dir=/astro/mwaeor/MWA/data
-out_dir=ws_im
+tag=POGSII-EG-024
+data_dir=/astro/mwaeor/achokshi/rm-synth/data
+out_dir=wsclean_im
 prefix=uvdump_
 
 
-cd "$data_dir"/"$obsid"/"$timestamp"
+cd "$data_dir"/"$obsid"/"$tag"
 mkdir -p $out_dir
 
 
-time wsclean -name ./"$out_dir"/uvdump_ -scale 0.75amin -size 2048 2048 \
+time wsclean -name ./"$out_dir"/uvdump -scale 0.75amin -size 256 256 \
   -niter 10000 -auto-threshold 0.5 -auto-mask 3 \
   -pol I -multiscale -weight briggs 0  -j 20 -mgain 0.85 \
   -no-update-model-required -abs-mem 100 \
   -grid-with-beam -use-idg -idg-mode cpu -pb-undersampling 4 \
   -mwa-path /astro/mwaeor/achokshi/software/local_python/mwa_pb/data \
-  "$data_dir"/"$obsid"/"$timestamp"/ms/uvdump_*.ms
+  "$data_dir"/"$obsid"/"$tag"/ms/"$prefix"*.ms
--- wsclean/sbatch_wsclean_rm.sh
+++ wsclean/sbatch_wsclean_rm.sh
@@ -1,21 +1,21 @@
 #!/bin/bash --login
 #SBATCH --nodes=1
 #SBATCH --partition=workq
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH --account=mwaeor
 #SBATCH --nodes=1
 #SBATCH --mem=100gb
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --output=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/wsclean-%A.out
-#SBATCH --error=/astro/mwaeor/MWA/data/1061316296/2021-01-28_2000/wsclean-%A.err
+#SBATCH --output=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/wsclean-rm-%A.out
+#SBATCH --error=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/wsclean-rm-%A.err
 
 module load wsclean
 
 obsid=1061316296
-tag=2021-01-31_0100
+tag=POGSII-EG-024
 data_dir=/astro/mwaeor/achokshi/rm-synth/data
-out_dir=ws_rm
+out_dir=wsclean_rm
 prefix=uvdump_
 
 
@@ -25,5 +25,5 @@ mkdir -p $out_dir
 
 time wsclean -pol QU -join-polarizations -join-channels \
   -squared-channel-joining --channels-out 768 \
-  -name ./"$out_dir"/uvdump -scale 0.75amin -size 2048 2048 \
-  "$data_dir"/"$obsid"/"$timestamp"/ms/uvdump_*.ms
+  -name ./"$out_dir"/uvdump -scale 0.75amin -size 256 256 \
+  ./ms/"$prefix"*.ms
