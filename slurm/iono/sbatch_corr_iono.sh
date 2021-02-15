58982fcbff2b991dbc08107f038d06d419a327b4
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-02-15 13:12:46 +0800

    Fixed iono paths

--- slurm/iono/sbatch_corr_iono.sh
+++ slurm/iono/sbatch_corr_iono.sh
@@ -13,7 +13,7 @@
 
 
 rts_dir=/astro/mwaeor/achokshi/rm-synth/data/1061316296/rts_imgr/rts_iii
-pogs_path=/astro/mwaeor/achokshi/rm-synth/iono/POGS-II_ExGal.fits
+pogs_path=/astro/mwaeor/achokshi/rm-synth/slurm/iono/POGS-II_ExGal.fits
 
 # Clean slate
 module purge
@@ -23,5 +23,5 @@ module use /astro/mwaeor/achokshi/software/modulefiles/
 module load rmextract-singularity
 
 
-time python /astro/mwaeor/achokshi/rm-synth/iono/corr_iono.py \
+time python /astro/mwaeor/achokshi/rm-synth/scripts/corr_iono.py \
     --fits_dir="$rts_dir"/stokes --out_dir="$rts_dir"/stokes_iono --pogs_path=$pogs_path
