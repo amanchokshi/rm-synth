8472ef06355dd95ff842e795736dd2b4e34ffedb
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-02-15 20:34:41 +0800

    Make out_dir if doesn't already exist. Update slurm script

--- slurm/cube/cube_rts.sh
+++ slurm/cube/cube_rts.sh
@@ -8,29 +8,15 @@
 #SBATCH --mem=30gb
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --output=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/fits2cube-%A.out
-#SBATCH --error=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/fits2cube-%A.err
+#SBATCH --output=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/cube_rts-%A.out
+#SBATCH --error=/astro/mwaeor/achokshi/rm-synth/data/slurm-logs/cube_rts-%A.err
 
 
-obsid=1061316296
-tag=patch
-data_dir=/astro/mwaeor/achokshi/rm-synth/data
-fits_dir="$data_dir"/"$obsid"/"$tag"/wsclean_rm
-prefix=uvdump
-suffix=dirty
-chans=768
-dim=256
-
+data_dir=/astro/mwaeor/achokshi/rm-synth/data/1061316296/rts_imgr/rts_iv
 
 module load python
 module load numpy
 module load astropy
 
-mkdir -p "$data_dir"/"$obsid"/"$tag"/rm_cubes
-
-time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
-    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=Q --chans=$chans --dim=$dim --out_dir="$data_dir"/"$obsid"/"$tag"/rm_cubes
-
-
-time python /astro/mwaeor/achokshi/rm-synth/wsclean/python-scripts/fits2cube.py \
-    --fits_dir=$fits_dir --prefix=$prefix --suffix=$suffix --pol=U --chans=$chans --dim=$dim --out_dir="$data_dir"/"$obsid"/"$tag"/rm_cubes
+time python /astro/mwaeor/achokshi/rm-synth/scripts/cube_rts.py \
+    --fits_dir="$data_dir"/stokes_iono --out_dir="$data_dir"/cubes
