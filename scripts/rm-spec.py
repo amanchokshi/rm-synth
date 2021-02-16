0277a3b0da96980f6f15fd072d5b1c3fd18896a7
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-02-16 23:31:58 +1100

    It's a beaut

--- scripts/rm-spec.py
+++ scripts/rm-spec.py
@@ -23,8 +23,8 @@ def pogs_obj_loc(pogs_obj, pogs_path):
 
     Returns
     -------
-    Astropy.coordinates.SkyCoord object
-        Ra, Dec of POGS object in radians
+    SkyCoord object
+        Astropy.coordinates.SkyCoord object
     """
 
     exgal = Table.read(pogs_path)
@@ -38,95 +38,136 @@ def pogs_obj_loc(pogs_obj, pogs_path):
     return pogs_pos
 
 
-#  # Read the POGs tables in to return an astropy Table object
-#  exgal = Table.read("../slurm/iono/POGS-II_ExGal.fits")
-#  df_eg = exgal.to_pandas()
-#  df_eg["catalog_id"] = df_eg["catalog_id"].str.decode("utf-8")
-#  df_eg.set_index("catalog_id", inplace=True)
+def read_rm_cube(pogs_pos, rm_prefix, cube_dir):
+    """Read rm fits cube created by cuFFS.
 
-#  pogs = df_eg.loc["POGSII-EG-321"]
-#  rm_peak = SkyCoord(pogs.ra, pogs.dec, unit="deg")
+    cuFFS creates spectral cubes with the weird
+    axis order. The first axis is phi, followed
+    by ra, dec. When the fits file is read by
+    astropy, the indexing order is reversed
 
-rm_peak = pogs_obj_loc("POGSII-EG-321", "../slurm/iono/POGS-II_ExGal.fits")
+    data == [δ, ⍺, 𝜙]
 
+    Parameters
+    ----------
+    pogs_pos : skycoord object
+        SkyCoord object from pogs_obj_loc
+    rm_prefix : str
+        Prefix to cube names as defined in cuFFS parset file
+    cube_dir: str
+        Path to directory with rm cubes
+    """
 
-##########################
-# data == [dec, ra, phi] #
-##########################
+    # Open cube and grab header and data
+    with fits.open(f"{cube_dir}/{rm_prefix}p.phi.dirty.fits") as hdus:
+        hdu = hdus[0]
+        hdr = hdu.header
+        data_p = hdu.data
 
-with fits.open("../data/rts_imgr_p.phi.dirty.fits") as hdus:
-    hdu = hdus[0]
-    hdr = hdu.header
-    data_p = hdu.data
+    # World Coordinate System
+    wcs = WCS(hdr)
 
-print(repr(hdr))
+    # Determine range of phi, ra, dec in data
+    # Here, the order of indexing the same as the fits file
+    # wcs.all_pix2world(𝜙, ⍺, δ, 0)
+    # The last 0 ensures pythonic zero indexing
+    phi, _, _ = wcs.all_pix2world(np.arange(data_p.shape[2]), 0, 0, 0)
+    _, ras, _ = wcs.all_pix2world(0, np.arange(data_p.shape[1]), 0, 0)
+    _, _, decs = wcs.all_pix2world(0, 0, np.arange(data_p.shape[0]), 0)
 
-#  with fits.open("../data/1086351512/rts_imgr/run_i/cubes/rts_imgr_q.phi.dirty.fits") as hdus:
-#  hdu = hdus[0]
-#  #  hdr = hdu.header
-#  data_q = hdu.data
+    # Determine pixel coordinates of pogs object
+    _, ra_x, dec_y = wcs.all_world2pix(0, pogs_pos.ra.deg, pogs_pos.dec.deg, 0)
 
-#  with fits.open("../data/1086351512/rts_imgr/run_i/cubes/rts_imgr_u.phi.dirty.fits") as hdus:
-#  hdu = hdus[0]
-#  hdr = hdu.header
-#  data_u = hdu.data
+    # Convert to closes integer pixel index
+    ra_x = np.round(ra_x).astype(int)
+    dec_y = np.round(dec_y).astype(int)
 
-wcs = WCS(hdr)
-phi, _, _ = wcs.all_pix2world(np.arange(data_p.shape[2]), 0, 0, 0)
-_, ras, _ = wcs.all_pix2world(0, np.arange(data_p.shape[1]), 0, 0)
-_, _, decs = wcs.all_pix2world(0, 0, np.arange(data_p.shape[0]), 0)
+    # Extract spectral slice of cube at location of pogs source
+    spec = data_p[np.round(dec_y).astype(int), np.round(ra_x).astype(int), :]
 
+    # Determine RM peak and index in cube
+    spec_peak = np.amax(spec)
+    phi_z = np.where(spec == spec_peak)[0][0]
 
-_, ra_x, dec_y = wcs.all_world2pix(0, rm_peak.ra.deg, rm_peak.dec.deg, 0)
+    print(f"Phi Max: {phi[phi_z]}, RM: {spec_peak:.3f}")
 
-ra_x = np.round(ra_x).astype(int)
-dec_y = np.round(dec_y).astype(int)
-
-spec = data_p[np.round(dec_y).astype(int), np.round(ra_x).astype(int), :]
-spec_peak = np.amax(spec)
-peak_idx = np.where(spec == spec_peak)[0][0]
-
-print(f"Phi Max: {phi[peak_idx]}, RM: {spec_peak:.3f}")
-
-plt.style.use("seaborn")
-
-ax = plt.subplot(projection=wcs[:, :, 355])
-im = ax.imshow(data_p[:, :, 355], origin="lower", cmap="viridis",)
-ax.plot(
-    np.round(ra_x).astype(int),
-    np.round(dec_y).astype(int),
-    "darkorange",
-    marker="o",
-    mfc="none",
-    ms=28,
-    mew=2.0,
-)
-
-ax.set_xlim(ra_x - 50, ra_x + 50)
-ax.set_ylim(dec_y - 50, dec_y + 50)
+    return ra_x, dec_y, phi_z, phi, data_p, wcs
 
 
-ax.coords.grid(True, color="white", ls="dotted")
-ax.coords[0].set_format_unit(u.deg)
-ax.coords[0].set_auto_axislabel(False)
-ax.set_xlabel("Right Ascension [deg]")
+if __name__ == "__main__":
 
-ax.coords[1].set_format_unit(u.deg)
-ax.coords[1].set_auto_axislabel(False)
-ax.set_ylabel("Declination [deg]")
+    pogs_pos = pogs_obj_loc("POGSII-EG-321", "../slurm/iono/POGS-II_ExGal.fits")
+    ra_x, dec_y, phi_z, phi, data_p, wcs = read_rm_cube(
+        pogs_pos, "rts_imgr_", "../data"
+    )
 
-plt.show()
+    plt.style.use("seaborn")
 
-plt.style.use("seaborn")
+    ax = plt.subplot(projection=wcs[:, :, int(phi_z)])
+    im = ax.imshow(data_p[:, :, int(phi_z)], origin="lower", cmap="viridis",)
+    ax.plot(
+        ra_x, dec_y, "darkorange", marker="o", mfc="none", ms=28, mew=2.0,
+    )
 
-plt.plot(
-    phi, data_p[np.round(dec_y).astype(int), np.round(ra_x).astype(int), :], label="p"
-)
-#  plt.plot(phi, data_q[128, 128, :], label="q")
-#  plt.plot(phi, data_u[128, 128, :], label="u")
-plt.xlabel("Faraday Depth")
-plt.ylabel("Polarized Flux Density")
-plt.legend()
-plt.tight_layout()
-#  plt.savefig("rm_spec.png")
-plt.show()
+    ax.set_xlim(ra_x - 50, ra_x + 50)
+    ax.set_ylim(dec_y - 50, dec_y + 50)
+
+    ax.coords.grid(True, color="white", ls="dotted")
+    ax.coords[0].set_format_unit(u.deg)
+    ax.coords[0].set_auto_axislabel(False)
+    ax.set_xlabel("Right Ascension [deg]")
+
+    ax.coords[1].set_format_unit(u.deg)
+    ax.coords[1].set_auto_axislabel(False)
+    ax.set_ylabel("Declination [deg]")
+
+    plt.show()
+
+    plt.style.use("seaborn")
+
+    plt.plot(phi, data_p[dec_y, ra_x, :], label="p", color="#207561")
+    plt.xlabel("Faraday Depth")
+    plt.ylabel("Polarized Flux Density")
+    plt.legend()
+    plt.tight_layout()
+    #  plt.savefig("rm_spec.png")
+    plt.show()
+
+    file = "../data/rts_imgr_rmsf.txt"
+    data = np.loadtxt(file)
+
+    phi = data[:, 0]
+    q = data[:, 1]
+    u = data[:, 2]
+    p = data[:, 3]
+
+    plt.style.use("seaborn")
+    plt.plot(phi, p, color="#207561", label=r"$ \vert R \vert $", zorder=2)
+    plt.plot(
+        phi,
+        q,
+        alpha=0.7,
+        color="#252525",
+        linewidth=1.8,
+        linestyle=(0, (0.1, 1.2)),
+        dash_capstyle="round",
+        label=r"$ real(R) $",
+        zorder=3,
+    )
+    plt.plot(
+        phi,
+        u,
+        alpha=0.7,
+        color="#350b40",
+        linewidth=1.6,
+        linestyle="dashed",
+        dash_capstyle="round",
+        label=r"$ imag(R) $",
+        zorder=1,
+    )
+    plt.xlabel("Faraday Depth")
+    plt.ylabel("RMSF")
+    plt.legend()
+    plt.tight_layout()
+    #  plt.savefig("rmsf.png")
+    plt.show()
