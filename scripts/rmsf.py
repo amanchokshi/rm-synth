452bdc2639c136fe533d0daeecef7bdd06d93a6f
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-02-16 23:30:35 +1100

    It's a beaut, but merging with rm-spec after this commit

--- scripts/rmsf.py
+++ scripts/rmsf.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-file = "../data/1086351512/rts_imgr/run_i/cubes/rts_imgr_rmsf.txt"
+file = "../data/rts_imgr_rmsf.txt"
 data = np.loadtxt(file)
 
 phi = data[:, 0]
@@ -11,12 +11,32 @@ p = data[:, 3]
 
 
 plt.style.use("seaborn")
-plt.plot(phi, p, label=r"$ \vert R \vert $")
-plt.plot(phi, q, label=r"$ real(R) $")
-plt.plot(phi, u, label=r"$ imag(R) $")
-#  plt.xlim([-100, 100])
+plt.plot(phi, p, color="#207561", linewidth=1.2, label=r"$ \vert R \vert $", zorder=3)
+plt.plot(
+    phi,
+    q,
+    alpha=0.7,
+    color="#da4302",
+    linewidth=2.8207561,
+    linestyle=(0, (0.1, 2)),
+    dash_capstyle="round",
+    label=r"$ real(R) $",
+    zorder=2
+)
+plt.plot(
+    phi,
+    u,
+    alpha=0.93,
+    color="#552244",
+    linewidth=1.6,
+    linestyle="dashed",
+    dash_capstyle="round",
+    label=r"$ imag(R) $",
+    zorder=1
+)
 plt.xlabel("Faraday Depth")
 plt.ylabel("RMSF")
 plt.legend()
 plt.tight_layout()
-plt.savefig("rmsf.png")
+#  plt.savefig("rmsf.png")
+plt.show()
