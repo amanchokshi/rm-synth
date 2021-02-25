a423319f56c3f091e98d567e048c9a0761ca466c
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-02-25 15:05:21 +1100

    Better formatting

--- scripts/plot_compare.py
+++ scripts/plot_compare.py
@@ -2,11 +2,12 @@
 Compare a set of RM Spectra
 """
 
-from plot_rm import pogs_obj_loc, read_rm_cube
-from matplotlib import pyplot as plt
-import matplotlib.patheffects as pe
-import matplotlib.pylab as pl
 import numpy as np
+from matplotlib import patheffects as pe
+from matplotlib import pylab as pl
+from matplotlib import pyplot as plt
+
+from plot_rm import pogs_obj_loc, read_rm_cube
 
 # Read POGS catalog and create source skycoord object
 pogs_pos = pogs_obj_loc("POGSII-EG-022", "../slurm/iono/POGS-II_ExGal.fits")
@@ -18,14 +19,23 @@ tags = ["f_p022", "f_p022_nodflag", "a_p022", "a_p022_nodflag"]
 
 plt.style.use("seaborn")
 
-colors = pl.cm.Spectral(np.linspace(0.05,0.9,len(tags)))
+colors = pl.cm.Spectral(np.linspace(0.05, 0.9, len(tags)))
 
 fig = plt.figure(figsize=(9, 6))
 ax = fig.add_subplot(1, 1, 1)
 
 for i, tag in enumerate(tags):
-    ra_x, dec_y, phi_z, phi, data_p, wcs = read_rm_cube(pogs_pos, "rts_imgr_", f"../data/{obsid}/{tag}/run_i/cubes_iono")
-    ax.plot(phi, data_p[dec_y, ra_x, :], label=f"{tag}", color=colors[i], alpha=0.8, path_effects=[pe.Stroke(linewidth=2.4, foreground="k"), pe.Normal()])
+    ra_x, dec_y, phi_z, phi, data_p, wcs = read_rm_cube(
+        pogs_pos, "rts_imgr_", f"../data/{obsid}/{tag}/run_i/cubes_iono"
+    )
+    ax.plot(
+        phi,
+        data_p[dec_y, ra_x, :],
+        label=f"{tag}",
+        color=colors[i],
+        alpha=0.8,
+        path_effects=[pe.Stroke(linewidth=2.4, foreground="k"), pe.Normal()],
+    )
 
 ax.set_xlabel("Faraday Depth [rad/m$^2$]")
 ax.set_ylabel("Polarized Flux Density [Jy/PSF/RMSF]")
