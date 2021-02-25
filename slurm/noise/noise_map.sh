0a1723b2571dde2d0ca504429c79c4bbe6149325
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-02-25 21:09:28 +0800

    Trying python singularity

--- slurm/noise/noise_map.sh
+++ slurm/noise/noise_map.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -l
 
 #SBATCH --nodes=1
 #SBATCH --partition=workq
-#SBATCH --time=00:20:00
+#SBATCH --time=01:00:00
 #SBATCH --account=mwaeor
 #SBATCH --nodes=1
 #SBATCH --mem=30gb
@@ -17,11 +17,9 @@ phi_mask=20
 cuffs_prefix=rts_imgr_
 cube_dir=/astro/mwaeor/achokshi/rm-synth/data/"$obsid"/"$1"/imgs/cubes_iono
 
-module load python
-module load numpy
-module load scipy
-module load astropy
-module load matplotlib
+module purge
+module use /pawsey/mwa/software/python3/modulefiles
+module load python-singularity
 
 time python /astro/mwaeor/achokshi/rm-synth/scripts/noise_map.py \
     --phi_mask="$phi_mask" --cuffs_prefix="$cuffs_prefix" --cube_dir="$cube_dir"
