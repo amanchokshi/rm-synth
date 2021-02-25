0601d9ffad0f60752dc678b63ac3bdf26eb84845
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-02-25 15:44:49 +1100

    Comparison of ana vs fee. Fix colors

--- scripts/plot_af.py
+++ scripts/plot_af.py
@@ -1,41 +1,47 @@
-from plot_rm import pogs_obj_loc, read_rm_cube
+from matplotlib import pyplot as plt
 
-if __name__ == "__main__":
+from plot_rm import pogs_obj_loc, read_noise, read_rm_cube
 
-    from matplotlib import pyplot as plt
+pogs_pos = pogs_obj_loc("POGSII-EG-321", "../slurm/iono/POGS-II_ExGal.fits")
 
-    #####################################################################
-    #                                                                   #
-    #                   Read in data and create spectra                 #
-    #                                                                   #
-    #####################################################################
+ra_a, dec_a, phi_a, phi_a, data_a, wcs_a = read_rm_cube(
+    pogs_pos, "ana_", "../data/noise_321"
+)
+n_a = read_noise(pogs_pos, "ana_", "../data/noise_321")
+ra_f, dec_f, phi_f, phi_f, data_f, wcs_f = read_rm_cube(
+    pogs_pos, "fee_", "../data/noise_321"
+)
+n_f = read_noise(pogs_pos, "fee_", "../data/noise_321")
 
-    pogs_pos = pogs_obj_loc("POGSII-EG-321", "../slurm/iono/POGS-II_ExGal.fits")
+plt.style.use("seaborn")
 
-    ra_x, dec_y, phi_z, phi, data_p, wcs = read_rm_cube(pogs_pos, "rts_imgr_", "../data/1086351512/rts_imgr/run_i/cubes")
-    ra_x_i, dec_y_i, phi_z_i, phi_i, data_p_i, wcs_i = read_rm_cube(pogs_pos, "rts_imgr_", "../data/1086351512/rts_imgr/run_i/cubes_iono")
+fig = plt.figure(figsize=(9, 6))
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(
+    phi_f,
+    data_f[dec_f, ra_f, :] - n_f,
+    label="FEE",
+    linewidth=2,
+    color="#28527a",
+    alpha=0.7,
+)
+ax.plot(
+    phi_a,
+    data_a[dec_a, ra_a, :] - n_a,
+    label="ANA",
+    linewidth=1,
+    linestyle="dashed",
+    dash_capstyle="round",
+    color="#ef4f4f",
+)
+ax.set_xlabel("Faraday Depth [rad/m$^2$]")
+ax.set_ylabel("Polarized Flux Density [Jy/PSF/RMSF]")
+ax.set_title("POGSII-EG-321 Analytic vs FEE Beam")
 
-    # Elegant Seaborn
-    plt.style.use("seaborn")
+leg = plt.legend(frameon=True, markerscale=1, handlelength=1)
+leg.get_frame().set_facecolor("white")
+for le in leg.legendHandles:
+    le.set_alpha(1)
 
-    #####################################################################
-    #                                                                   #
-    #                  Plot RM spectra of POGS source                   #
-    #                                                                   #
-    #####################################################################
-
-    fig = plt.figure(figsize=(9, 6))
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(phi, data_p[dec_y, ra_x, :], label="Raw RM", color="#822659", alpha=0.8)
-    ax.plot(phi_i, data_p_i[dec_y_i, ra_x_i, :], label="Iono Corr RM", color="#207561")
-    ax.set_xlabel("Faraday Depth [rad/m$^2$]")
-    ax.set_ylabel("Polarized Flux Density [Jy/PSF/RMSF]")
-    ax.set_title("POGSII-EG-321 Ionospheric RM effects")
-
-    leg = plt.legend(frameon=True, markerscale=1, handlelength=1)
-    leg.get_frame().set_facecolor("white")
-    for le in leg.legendHandles:
-        le.set_alpha(1)
-
-    plt.tight_layout()
-    plt.savefig(f"rm_spec.png", dpi=300)
+plt.tight_layout()
+plt.savefig("rm_spec_ana_fee.png", dpi=300)
