53b1bebe5c7ef9f7b11e73950a6ebe69e8e3e7d8
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-03-19 12:16:59 +0800

    Batch cuffs scripts

--- slurm/cuffs/batch_cuffs.sh
+++ slurm/cuffs/batch_cuffs.sh
@@ -4,7 +4,9 @@ for i in 1120300232 1120300352 1120082744 1120082864;
 do
     for j in ana_wide fee_wide;
     do
-        echo Submitting cube_rts.sh "$i" "$j" to slurm queue
-        sbatch cube_rts.sh $i $j
+        echo Submitting cuffs.sh "$i" "$j" to slurm queue
+        sed "s/obsid/"$i"/g" cuffs_parset.in >> "$j"_"$i".in
+        sed -i "s/tag/"$j"/g" "$j"_"$i".in
+        sbatch cuffs.sh "$j"_"$i".in
     done;
 done;
