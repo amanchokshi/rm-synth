3caddf041409906eb309e56f90588cdfa03fa979
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-05-18 10:55:28 +1000

    Added makeUnpolResponse function to beam_utils

--- scripts/beam_utils.py
+++ scripts/beam_utils.py
@@ -1,5 +1,31 @@
-import numpy as np
 import healpy as hp
+import numpy as np
+
+
+def makeUnpolInstrumentalResponse(j1, j2):
+    """Convert Jones matracies to unpolarised beam responses.
+
+       Form the visibility matrix in instrumental response from two Jones
+       matrices assuming unpolarised sources (hence the brightness matrix is
+       the identity matrix)
+
+       Input: j1,j2: Jones matrices of dimension[za][az][2][2]
+       Returns: [za][az][[xx,xy],[yx,yy]] where "X" and "Y" are defined by the receptors
+       of the Dipole object used in the ApertureArray. Hence to get "XX", you want
+       result[za][az][0][0] and for "YY" you want result[za][az][1][1]
+
+       Modified from:
+       https://github.com/MWATelescope/mwa_pb/blob/696c2835f44de510da2d5d5bcd3c15223bbe6d7b/mwa_pb/beam_tools.py
+
+       output: [XX, XY, YX, YY]
+    """
+    result = np.empty_like(j1)
+
+    result[:, 0] = j1[:, 0] * j2[:, 0].conjugate() + j1[:, 1] * j2[:, 1].conjugate()
+    result[:, 3] = j1[:, 2] * j2[:, 2].conjugate() + j1[:, 3] * j2[:, 3].conjugate()
+    result[:, 1] = j1[:, 0] * j2[:, 2].conjugate() + j1[:, 1] * j2[:, 3].conjugate()
+    result[:, 2] = j1[:, 2] * j2[:, 0].conjugate() + j1[:, 3] * j2[:, 1].conjugate()
+    return result
 
 
 def healpix_za_az(nside=32):
