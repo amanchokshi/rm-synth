b1c451cb4b00ba17d035064d237b46d5397cea54
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-05-24 12:01:46 +1000

    This seems to work remarkably well for complex initial parameters

--- scripts/beam_minimize.py
+++ scripts/beam_minimize.py
@@ -4,7 +4,6 @@ import mwa_hyperbeam
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.optimize import minimize
-from scipy.stats import median_abs_deviation as mad
 
 import beam_utils as bu
 
@@ -20,7 +19,7 @@ def likelihood(amps, data):
     Returns
     -------
     :float
-        The probability that the model fits the data
+        The inverse log probability that the model fits the data
     """
 
     # Make a new beam object
@@ -51,7 +50,6 @@ def likelihood(amps, data):
     model_XX = np.real(unpol_beam[:, 0])
     #  model_YY = np.real(unpol_beam[:, 3])
 
-    #  chisq = np.sum(np.square(data - model_XX) / mad(data - model_XX))
     chisq = np.sum(np.square(data - model_XX))
 
     return np.log(chisq)
@@ -78,48 +76,30 @@ if __name__ == "__main__":
     norm_to_zenith = True
 
     # Create synthetic data at try to recover the input parameters
-    #  amps_15 = [0.4] * 1 + [0.8] * 1 + [1.0] * 14
-    # amps_15 = [
-    #     0.2,
-    #     0.3,
-    #     0.4,
-    #     0.5,
-    #     0.6,
-    #     0.7,
-    #     0.8,
-    #     0.9,
-    #     0.9,
-    #     0.8,
-    #     0.7,
-    #     0.6,
-    #     0.5,
-    #     0.4,
-    #     0.3,
-    #     0.2,
-    # ]
-
-    amps_15 = [0.5] + [1.0] * 15
+    amps_15 = [
+        0.2,
+        0.3,
+        0.4,
+        0.5,
+        0.6,
+        0.7,
+        0.8,
+        0.9,
+        0.9,
+        0.8,
+        0.7,
+        0.6,
+        0.5,
+        0.4,
+        0.3,
+        0.2,
+    ]
 
     jones_15 = beam.calc_jones_array(az, za, freq, delays, amps_15, norm_to_zenith)
     unpol_beam_15 = bu.makeUnpolInstrumentalResponse(jones_15, jones_15)
 
     data_XX_15 = np.real(unpol_beam_15[:, 0])
     # data_YY_15 = np.real(unpol_beam_15[:, 3])
-
-    # lik = []
-    # for i in np.linspace(0, 1, 101):
-    #     amps = [i] + [1.0] * 15
-
-    #     log_lik = likelihood(amps, data_XX_15)
-
-    #     lik.append(log_lik)
-
-    # plt.style.use("seaborn")
-    # plt.plot(np.linspace(0, 1, 101), lik, "-o", color="midnightblue")
-    # plt.xlabel("$A_0$ Amplitude")
-    # plt.ylabel("Liklihood")
-    # plt.tight_layout()
-    # plt.show()
 
     # Our walkers will be centralised to this location
     amps_guess = [0.5] * 16
