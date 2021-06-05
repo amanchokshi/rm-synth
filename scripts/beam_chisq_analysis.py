0b20efa6f5406308d47ea47c709c185a9f32e77c
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-06-06 01:34:33 +1000

    Modified for new data structure

--- scripts/beam_chisq_analysis.py
+++ scripts/beam_chisq_analysis.py
@@ -2,46 +2,9 @@ import healpy as hp
 import mwa_hyperbeam
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy import stats
 
 import beam_utils as bu
-
-
-def plot_beam_amp(beam_min, dipoles, index, fig, ax):
-
-    cols = plt.cm.Spectral(np.linspace(0, 1, 16))
-    kde = stats.gaussian_kde(beam_min[:, index])
-    ax.hist(
-        beam_min[:, index],
-        bins=np.arange(0.4, 1.02, 0.02),
-        ec="black",
-        density=True,
-        color=cols[index],
-        alpha=0.99,
-        label=f"{dipoles[index]} : $\mu$={np.median(beam_min[:, index]):.2f}",
-    )
-    ax.plot(
-        np.linspace(min(beam_min[:, index]), max(beam_min[:, index]), 128),
-        kde(np.linspace(min(beam_min[:, index]), max(beam_min[:, index]), 128)),
-        linewidth=2,
-    )
-    ax.vlines(
-        np.median(beam_min[:, index]),
-        0,
-        kde(np.median(beam_min[:, index])),
-        color="black",
-        linewidth=2,
-        linestyle="dashed",
-    )
-    ax.set_xlim([0.4, 1.02])
-    #  ax.set_ylim([0.0, 7])
-    ax.set_xlabel("Dipole Gain Amplitude [0, 1]")
-    #  ax.set_yticks([])
-
-    leg = ax.legend(loc="upper left", frameon=True, markerscale=4, handlelength=1)
-    leg.get_frame().set_facecolor("white")
-    for le in leg.legendHandles:
-        le.set_alpha(0.9)
+from beam_minimize import beam_mask
 
 
 def log_chisq(model, data, mask):
@@ -55,103 +18,47 @@ if __name__ == "__main__":
 
     from pathlib import Path
 
-    #  beam_min = np.load("../data/mcmc/S06XX_beam_min_1024_walk.npy")
     tiles = [
-        "S06XX",
-        "S06YY",
-        "S07XX",
-        "S07YY",
-        "S08XX",
-        "S08YY",
-        "S09XX",
-        "S09YY",
-        "S10XX",
-        "S10YY",
-        "S12XX",
-        "S12YY",
-        "S29XX",
-        "S29YY",
-        "S30XX",
-        "S30YY",
-        "S31XX",
-        "S31YY",
-        "S32XX",
-        "S32YY",
-        "S33XX",
-        "S33YY",
-        "S34XX",
-        "S34YY",
-        "S35XX",
-        "S35YY",
-        "S36XX",
-        "S36YY",
+        "S06XX_rf1XX",
+        "S06YY_rf1YY",
+        "S07XX_rf1XX",
+        "S07YY_rf1YY",
+        "S08XX_rf1XX",
+        "S08YY_rf1YY",
+        "S09XX_rf1XX",
+        "S09YY_rf1YY",
+        "S10XX_rf1XX",
+        "S10YY_rf1YY",
+        "S12XX_rf1XX",
+        "S12YY_rf1YY",
+        "S29XX_rf1XX",
+        "S29YY_rf1YY",
+        "S30XX_rf1XX",
+        "S30YY_rf1YY",
+        "S31XX_rf1XX",
+        "S31YY_rf1YY",
+        "S32XX_rf1XX",
+        "S32YY_rf1YY",
+        "S33XX_rf1XX",
+        #  "S33YY_rf1YY",
+        "S34XX_rf1XX",
+        #  "S34YY_rf1YY",
+        "S35XX_rf1XX",
+        "S35YY_rf1YY",
+        "S36XX_rf1XX",
+        "S36YY_rf1YY",
     ]
 
     chi_sq_tiles = []
 
     for tile in tiles:
 
-        out_dir = Path(f"../data/mcmc/beam_min_masked_512/{tile}/")
+        out_dir = Path(f"../data/beam_min_1024_masked/{tile}/")
         out_dir.mkdir(parents=True, exist_ok=True)
 
         beam_min = np.load(
-            f"../data/mcmc/beam_min_masked_512/data/{tile}_beam_min_512_walk_mask.npy"
+            f"../data/beam_min_1024_masked/raw/{tile}_beam_min_1024_walk_mask.npy"
         )
-
-        dipoles = [
-            "$A_0$",
-            "$A_1$",
-            "$A_2$",
-            "$A_3$",
-            "$A_4$",
-            "$A_5$",
-            "$A_6$",
-            "$A_7$",
-            "$A_8$",
-            "$A_9$",
-            "$A_{10}$",
-            "$A_{11}$",
-            "$A_{12}$",
-            "$A_{13}$",
-            "$A_{14}$",
-            "$A_{15}$",
-        ]
-
-        plt.style.use("seaborn")
-
-        plt.rcParams.update(
-            {
-                "font.size": 16,
-                "font.family": "serif",
-                "font.serif": "Times New Roman",
-                "text.usetex": True,
-            }
-        )
-        fig, axs = plt.subplots(4, 4, sharex=True, sharey=True, figsize=(16, 9))
-        plot_beam_amp(beam_min, dipoles, 0, fig, axs[0, 0])
-        plot_beam_amp(beam_min, dipoles, 1, fig, axs[0, 1])
-        plot_beam_amp(beam_min, dipoles, 2, fig, axs[0, 2])
-        plot_beam_amp(beam_min, dipoles, 3, fig, axs[0, 3])
-        plot_beam_amp(beam_min, dipoles, 4, fig, axs[1, 0])
-        plot_beam_amp(beam_min, dipoles, 5, fig, axs[1, 1])
-        plot_beam_amp(beam_min, dipoles, 6, fig, axs[1, 2])
-        plot_beam_amp(beam_min, dipoles, 7, fig, axs[1, 3])
-        plot_beam_amp(beam_min, dipoles, 8, fig, axs[2, 0])
-        plot_beam_amp(beam_min, dipoles, 9, fig, axs[2, 1])
-        plot_beam_amp(beam_min, dipoles, 10, fig, axs[2, 2])
-        plot_beam_amp(beam_min, dipoles, 11, fig, axs[2, 3])
-        plot_beam_amp(beam_min, dipoles, 12, fig, axs[3, 0])
-        plot_beam_amp(beam_min, dipoles, 13, fig, axs[3, 1])
-        plot_beam_amp(beam_min, dipoles, 14, fig, axs[3, 2])
-        plot_beam_amp(beam_min, dipoles, 15, fig, axs[3, 3])
-
-        # Only outer labels
-        for ax in fig.get_axes():
-            ax.label_outer()
-
-        plt.tight_layout()
-        plt.savefig(f"{out_dir}/{tile}_beam_min.png")
-        plt.close()
 
         amps_sat = np.median(beam_min, axis=0)
 
@@ -173,11 +80,11 @@ if __name__ == "__main__":
             pol = "YY"
 
         # Load satellite beam map
-        data_sat = np.load(f"../data/embers_healpix/{tile}_rf1{pol}_0.npz")["beam_map"][
-            : az.shape[0]
-        ]
+        map_med = np.load(f"../data/embers_maps/rf1_med_maps/{tile}_med.npy")
+        map_mad = np.load(f"../data/embers_maps/rf1_mad_maps/{tile}_mad.npy")
 
-        # Create mask based on -30dB threshold of perfect FEE model
+        mask = beam_mask(map_med, map_mad, pol=pol)
+
         jones_perfect = beam.calc_jones_array(
             az, za, freq, delays, [1.0] * 16, norm_to_zenith
         )
@@ -185,10 +92,11 @@ if __name__ == "__main__":
 
         if pol == "XX":
             fee_perfect = 10 * np.log10(np.real(unpol_perfect[:, 0]))
-            mask_30dB = np.where(fee_perfect < -30)
         else:
             fee_perfect = 10 * np.log10(np.real(unpol_perfect[:, 3]))
-            mask_30dB = np.where(fee_perfect < -30)
+
+        fee = np.zeros(hp.nside2npix(nside))
+        fee[: fee_perfect.shape[0]] = fee_perfect
 
         jones_min = beam.calc_jones_array(
             az, za, freq, delays, amps_sat, norm_to_zenith
@@ -196,14 +104,21 @@ if __name__ == "__main__":
         unpol_min = bu.makeUnpolInstrumentalResponse(jones_min, jones_min)
 
         if pol == "XX":
-            fee_min = 10 * np.log10(np.real(unpol_min[:, 0]))
+            data_min = 10 * np.log10(np.real(unpol_min[:, 0]))
         else:
-            fee_min = 10 * np.log10(np.real(unpol_min[:, 3]))
+            data_min = 10 * np.log10(np.real(unpol_min[:, 3]))
 
-        fee_chisq = log_chisq(fee_perfect, data_sat, mask_30dB)
-        fee_min_chisq = log_chisq(fee_min, data_sat, mask_30dB)
+        fee_min = np.zeros(hp.nside2npix(nside))
+        fee_min[: data_min.shape[0]] = data_min
 
-        chi_sq_tiles.append([fee_chisq, fee_min_chisq])
+        fee[mask] = np.nan
+        fee_min[mask] = np.nan
+        map_med[mask] = np.nan
+        map_mad[mask] = np.nan
+        log_chisq_fee = np.log(np.nansum(np.square(fee - map_med) / map_mad))
+        log_chisq_fee_min = np.log(np.nansum(np.square(fee_min - map_med) / map_mad))
+
+        chi_sq_tiles.append([log_chisq_fee, log_chisq_fee_min])
 
     # Hyperbeam settings
     nside = 32
@@ -241,6 +156,7 @@ if __name__ == "__main__":
         log_chi_sq.append(lchs)
 
     chi_sq_tiles = np.array(chi_sq_tiles)
+
     plt.style.use("seaborn")
     plt.rcParams.update(
         {
@@ -251,18 +167,62 @@ if __name__ == "__main__":
         }
     )
     fig, ax = plt.subplots(1, 1, figsize=(11, 7))
-    ax.plot(range(28), chi_sq_tiles[:, 0], "-D", color="#008891", label="FEE Perfect")
-    ax.plot(range(28), chi_sq_tiles[:, 1], "-s", color="#00587a", label="FEE Minimized")
-    ax.hlines(
-        np.mean(log_chi_sq),
-        0,
-        27,
-        colors="#687980",
-        linestyles="dashed",
-        label="Avg Flagged Dipole",
+    ax.plot(
+        range(len(tiles)),
+        chi_sq_tiles[:, 0],
+        "-D",
+        color="#008891",
+        label="FEE Perfect",
     )
-    ax.set_xticks(np.arange(28))
-    ax.set_xticklabels(tiles)
+    ax.plot(
+        range(len(tiles)),
+        chi_sq_tiles[:, 1],
+        "-s",
+        color="#00587a",
+        label="FEE Minimized",
+    )
+    # ax.hlines(
+    #     np.mean(log_chi_sq),
+    #     0,
+    #     len(tiles) - 1,
+    #     colors="#687980",
+    #     linestyles="dashed",
+    #     label="Avg Flagged Dipole",
+    # )
+
+    tile_names = [
+        "$S06XX$",
+        "$S06YY$",
+        "$S07XX$",
+        "$S07YY$",
+        "$S08XX$",
+        "$S08YY$",
+        "$S09XX$",
+        "$S09YY$",
+        "$S10XX$",
+        "$S10YY$",
+        "$S12XX$",
+        "$S12YY$",
+        "$S29XX$",
+        "$S29YY$",
+        "$S30XX$",
+        "$S30YY$",
+        "$S31XX$",
+        "$S31YY$",
+        "$S32XX$",
+        "$S32YY$",
+        "$S33XX$",
+        #  "$S33YY$",
+        "$S34XX$",
+        #  "$S34YY$",
+        "$S35XX$",
+        "$S35YY$",
+        "$S36XX$",
+        "$S36YY$",
+    ]
+
+    ax.set_xticks(range(len(tile_names)))
+    ax.set_xticklabels(tile_names)
     ax.tick_params(axis="x", rotation=90)
     ax.set_ylabel("Log Chi Square")
     ax.set_title("MWA Satellite Beam Map Gain Minimization : Log Chi Square Test")
@@ -273,8 +233,8 @@ if __name__ == "__main__":
         le.set_alpha(1)
 
     plt.tight_layout()
-    plt.savefig("../data/mcmc/beam_min_masked_512/MWA_Min_Log_Chisq.png")
     #  plt.show()
+    plt.savefig("../data/beam_min_1024_masked/MWA_Min_Log_Chisq.png")
 
     # dipoles = [
     #     "$A_0$",
@@ -305,6 +265,6 @@ if __name__ == "__main__":
     #     }
     # )
     # fig = plt.subplots(1, 1, figsize=(11, 7))
-    # plt.plot(range(16), log_chi_sq, "-o", color="midnightblue")
+    # plt.plot(range(len(dipoles)), log_chi_sq, "-o", color="midnightblue")
     # plt.tight_layout()
-    # plt.savefig("../data/mcmc/beam_min_masked_512/Flagged_FEE_Log_Chisq.png")
+    # plt.savefig("../data/beam_min_1024_masked/Flagged_FEE_Log_Chisq.png")
