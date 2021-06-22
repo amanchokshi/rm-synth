14a4963406e4f8fb978a5cbd7493ba181348c0c0
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-06-22 18:22:08 +1000

    Try on ucal - included chris' memory suggestions

--- scripts/beam_min_combinations.py
+++ scripts/beam_min_combinations.py
@@ -5,13 +5,15 @@ import itertools
 import json
 from pathlib import Path
 
+import healpy as hp
+import mwa_hyperbeam
 import numpy as np
 from scipy import stats
 from scipy.signal import find_peaks
 
-from beam_minimize import beam_mask, likelihood
+import beam_utils as bu
 
-from tqdm import tqdm
+# from tqdm import tqdm
 
 
 def write_json(data, filename=None, out_dir=None):
@@ -25,6 +27,138 @@ def write_json(data, filename=None, out_dir=None):
 
     with open(f"{out_dir}/{filename}", "w") as f:
         json.dump(data, f, indent=4)
+
+
+def beam_mask(
+    hyperbeam, med_map, mad_map, pol=None, db_thresh=-30, zen_mask=20, nside=32
+):
+    """Create the ultimate beam mask.
+
+    The satellite beam map needs to be maskes in various ways before
+    it can be used for minimization.
+
+     - Mask the nulls below `db_thresh` from zenith power
+     - Mask the central `zen_mask` degrees
+     - Mask NaNs in the Median satellite map
+     - Mask zeros in MAD map - indicative of single satellite pass in the pixel
+
+    Parameter
+    ---------
+    hyperbeam : :class:`mwa_hyperbeam.FEEBeam()`
+        Instance of FEEBeam class
+    med_map : numpy.array
+        Healpix satellite median map
+    mad_map : numpy.array
+        Healpix satellite MAD map - noise
+    pol : string
+        Polarization of map - XX / YY
+    db_thresh : int / float
+        Null mask power threshold - default: -30dB
+    zen_mask : int / float
+        Central radii in degrees to mask - default: 20
+    nside : int
+        Healpix nside - default: 32
+
+    Returns
+    -------
+    :numpy.array
+        Numpy array of indicies of nside healpix map to be masked
+
+    """
+
+    # Make a new beam object
+    # beam = mwa_hyperbeam.FEEBeam()
+
+    # Hyperbeam settings
+    freq = 138e6
+    delays = [0] * 16
+    norm_to_zenith = True
+
+    # Zenith angle and Azimuth of healpix pixels
+    za, az = bu.healpix_za_az(nside=nside)
+
+    jones_perfect = beam.calc_jones_array(
+        az, za, freq, delays, [1.0] * 16, norm_to_zenith
+    )
+    unpol_perfect = bu.makeUnpolInstrumentalResponse(jones_perfect, jones_perfect)
+
+    if pol == "XX":
+        fee_hyperbeam = 10 * np.log10(np.real(unpol_perfect[:, 0]))
+    else:
+        fee_hyperbeam = 10 * np.log10(np.real(unpol_perfect[:, 3]))
+
+    # Anything below 30dB from peak of FEE zenith norm beam
+    mask_dB = np.where(fee_hyperbeam < db_thresh)[0]
+
+    # The central pixels upto `zen_mask` degrees
+    zenith_mask = np.arange(hp.ang2pix(nside, np.deg2rad(zen_mask), 0))
+
+    # All nans in sat median map
+    nan_mask = np.where(np.isnan(med_map) == True)[0]
+
+    # All 0 values in MAD array - indicative of only single satellite pass
+    mad_mask = np.where(mad_map == 0.0)[0]
+
+    mask = np.unique(np.concatenate((zenith_mask, mask_dB, nan_mask, mad_mask)))
+
+    return mask
+
+
+def likelihood(hyperbeam, amps, med_map, mad_map, mask, pol):
+    """Likelihood of a beam model give some data.
+
+    Parameter
+    ---------
+    hyperbeam : :class:`mwa_hyperbeam.FEEBeam()`
+        Instance of FEEBeam class
+    amps : numpy.array
+        16 element dipole amplitude array
+    data : numpy.array
+        Satellite beam model healpix array
+    mask : numpy.array
+        Indicies of data array where beam power >= -30dB
+    pol : string
+        Either XX, YY, indicating the polarization of current map
+
+    Returns
+    -------
+    :float
+        The log probability that the model fits the data
+    """
+
+    # Make a new beam object
+    # beam = mwa_hyperbeam.FEEBeam()
+
+    # Hyperbeam settings
+    nside = 32
+    freq = 138e6
+    delays = [0] * 16
+    norm_to_zenith = True
+
+    # Zenith angle and Azimuth of healpix pixels
+    za, az = bu.healpix_za_az(nside=nside)
+
+    # Indicies of za, az arrays to be masked and not evaluated
+    mask_za_az = mask[np.where(mask < az.shape[0])]
+    za = np.delete(za, mask_za_az)
+    az = np.delete(az, mask_za_az)
+
+    # Create model with given amplitudes
+    jones = beam.calc_jones_array(az, za, freq, delays, amps, norm_to_zenith)
+    unpol_beam = bu.makeUnpolInstrumentalResponse(jones, jones)
+
+    if pol == "XX":
+        model = 10 * np.log10(np.real(unpol_beam[:, 0]))
+    else:
+        model = 10 * np.log10(np.real(unpol_beam[:, 3]))
+
+    # Remove masked data from sat data maps
+    med_map = np.delete(med_map, mask)
+    mad_map = np.delete(mad_map, mask)
+
+    chisq = np.sum(np.square(med_map - model) / mad_map)
+
+    return np.log(chisq)
 
 
 def peak_amps(beam_min_npy):
@@ -73,7 +207,7 @@ def amp_combinations(amps):
     return amps_16
 
 
-def amp_comb_chisq(tile):
+def amp_comb_chisq(hyperbeam, tile):
     """Determine chisq for all combinations of dipole amps."""
 
     print(tile)
@@ -86,6 +220,7 @@ def amp_comb_chisq(tile):
     amps_16 = amp_combinations(p_amps)
 
     amps_chisq = {}
+    # for i, a16 in enumerate(tqdm(amps_16)):
     for i, a16 in enumerate(amps_16):
 
         if "XX" in tile:
@@ -97,9 +232,9 @@ def amp_comb_chisq(tile):
         map_med = np.load(f"../data/embers_maps/rf1_med_maps/{tile}_med.npy")
         map_mad = np.load(f"../data/embers_maps/rf1_mad_maps/{tile}_mad.npy")
 
-        mask = beam_mask(map_med, map_mad, pol=pol)
+        mask = beam_mask(hyperbeam, map_med, map_mad, pol=pol)
 
-        chi = likelihood(a16, map_med, map_mad, mask, pol)
+        chi = likelihood(hyperbeam, a16, map_med, map_mad, mask, pol)
 
         amps_chisq[i] = [list(a16), chi]
 
@@ -139,7 +274,10 @@ if __name__ == "__main__":
         "S36YY_rf1YY",
     ]
 
-    # with concurrent.futures.ProcessPoolExecutor(max_workers=7) as executor:
-    #     results = executor.map(amp_comb_chisq, tiles)
+    # Make a new beam object
+    beam = mwa_hyperbeam.FEEBeam()
 
-    amp_comb_chisq(tiles[0])
+    with concurrent.futures.ProcessPoolExecutor(max_workers=7) as executor:
+        results = executor.map(amp_comb_chisq, itertools.repeat(beam), tiles)
+
+    #  amp_comb_chisq(beam, tiles[0])
