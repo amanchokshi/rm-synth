bb2cc234b6c164aaaea506e28d1a235aa024db6c
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-06-22 17:54:02 +1000

    Removed print

--- scripts/beam_min_combinations.py
+++ scripts/beam_min_combinations.py
@@ -141,6 +141,3 @@ if __name__ == "__main__":
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         results = executor.map(amp_comb_chisq, tiles)
-
-    for result in results:
-        print(result)
